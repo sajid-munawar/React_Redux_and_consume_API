d27f69cf4d400c082af72a838c6f3c2a017be5ff
sajid-munawar <sajidmunawar666@gmail.com>
2022-08-04 17:12:08 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const dispatch = useDispatch()
   return (
     <div className="App">
-      <h1>This is simple react app use redux state and store</h1>
+      <h1>This is simple react app use redux state and  store</h1>
       <h2>counter = {counts}</h2>
       <button onClick={()=>dispatch(increment())}>Increment</button>
       <button onClick={()=>dispatch(decrement())}>Decrement</button>
